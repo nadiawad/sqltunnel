4e280512f5fe63bcd5a2dc00f8c5c8bd610d2fbf
Alan McGinlay <amcginlay@users.noreply.github.com>
2017-08-02 08:42:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 require('http').createServer(function(req, res) {
 		res.writeHead(200, {'Content-Type' : 'text/plain'});
-		res.end('SQL Tunnel\n\n' + (process.env.VCAP_SERVICES || 'no service bindings') + '\n');
+		res.end('SQL Tunnel (e.g. cf ssh -L 63306:10.193.144.75:3306 sqltunnel)\n\n' + (process.env.VCAP_SERVICES || 'no service bindings') + '\n');
 }).listen(process.env.PORT || 8000);
