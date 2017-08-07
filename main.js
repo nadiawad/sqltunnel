c1fbd3e833cc48c8ecc694bf0ba22054693214b0
Alan McGinlay <amcginlay@pivotal.io>
2017-08-07 22:49:19 +0100

    security fix

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 require('http').createServer(function(req, res) {
-		res.writeHead(200, {'Content-Type' : 'text/plain'});
-		res.end('SQL Tunnel (e.g. cf ssh -L 63306:10.193.144.75:3306 sqltunnel)\n\n');
+		res.writeHead(200, {'Content-Type' : 'text/html'});
+		res.write('<!DOCTYPE html>');
+		res.write('<html>');
+		res.write('<head>');
+		res.write('<meta charset="UTF-8">');
+		res.write('<title>SQL Tunnel</title>');
+		res.write('</head>');
+		res.write('<body>');
+		res.write('	<a href="https://github.com/amcginlay/sqltunnel/blob/master/README.md">SQL Tunnel</a>');
+		res.write('</body>');
+		res.end('</html>');
 }).listen(process.env.PORT || 8000);
